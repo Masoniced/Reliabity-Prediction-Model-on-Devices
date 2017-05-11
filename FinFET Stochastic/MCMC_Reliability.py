03a822478cf422a419deb1e2b0d2aaf0579c4242
mason <sen_mei@mymail.sutd.edu.sg>
2017-05-11 12:34:14 +0800

    update

--- FinFET Stochastic/MCMC_Reliability.py
+++ FinFET Stochastic/MCMC_Reliability.py
@@ -1,9 +1,63 @@
 import numpy as np
 import pandas as pd
 import sympy as sy
+import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from math import *
-import xlrd
+import sys
+
+class MCMC:
+
+    def MC_sampler(data, burn_in, tol, num_cluster=None):
+
+        # Initialization
+        num_data, dim_data = np.shape(data)
+        cluster_index = [[] for i in range(num_cluster)]
+        cluster_data = [[] for i in range(num_cluster)]
+        if num_cluster == None:
+            num_cluster = 2
+        ini_list = np.random.multinomial(num_data, [1/num_cluster]*num_cluster, size=1)
+        ini_index = np.arange(0, num_data, 1)
+        for i in range(num_cluster):
+            for j in range(ini_list[0, i]):
+                t_ind =np.round(np.random.uniform(0, len(ini_index)-1)).astype(int)
+                cluster_index[i].append(ini_index[t_ind])
+                ini_index = np.delete(ini_index, t_ind)
+            
+        # Posterior Distribution sum(w_model*(theta*(s)**(alpha-1)*alpha*exp(-theta*(s)**alpha))
+        theta_alpha = 0.001
+        theta_beta = 0.001
+        alpha_alpha = 1
+        alpha_beta = 0.2
+        w_fa = np.ones(num_cluster)
+        w_model = np.random.dirichlet(w_fa)
+        theta = np.random.gamma(theta_alpha, theta_beta, num_cluster)
+        alpha = np.random.gamma(alpha_alpha, alpha_beta, num_cluster)
+
+        # Update of P(z)
+        for i in range(num_data):
+            pz_list = np.array([w_model[k] * (theta[k]*alpha[k]*data[i]**(alpha[k]-1)) * exp(-theta[k]*data[i]**(alpha[k])) for k in range(num_cluster)])
+            pz_list = np.cumsum(pz_list)/sum(pz_list)
+            mv_pz, ind_pz = min([(mv_pz, ind_pz) for ind_pz, mv_pz in enumerate(abs(pz_list-np.random.uniform()))])
+            cluster_data[ind_pz].append(data[i])
+
+        # Update of w_model
+        for i in range(num_cluster):
+            w_model[i] = np.random.dirichlet(w_fa[i] + len(cluster_data[i]))
+
+        # Update of theta
+        for i in range(num_cluster):
+            new_theta_alpha = len(cluster_data[i]) + theta_alpha
+            new_theta_beta = theta_beta + sum([cluster_data[i][k]**(alpha[i]) for k in range(len(cluster_data[i]))])
+            theta[i] = np.random.gamma(new_theta_alpha, new_theta_beta)
+
+        # Update of alpha
+
+        
+
+
+
+
 
 def sampler(data, samples=4, mu_init=.5, proposal_width=.5, plot=False, mu_prior_mu=0, mu_prior_sd=1.):
     mu_current = mu_init
@@ -40,6 +94,96 @@ def sampler(data, samples=4, mu_init=.5, proposal_width=.5, plot=False, mu_prior
         posterior.append(mu_current)
         
     return posterior
+
+
+def sampler(data, samples=4, mu_init=.5, proposal_width=.5, plot=False, mu_prior_mu=0, mu_prior_sd=1.):
+    mu_current = mu_init
+    posterior = [mu_current]
+    for i in range(samples):
+        # suggest new position
+        mu_proposal = norm(mu_current, proposal_width).rvs()
+
+        # Compute likelihood by multiplying probabilities of each data point
+        likelihood_current = norm(mu_current, 1).pdf(data).prod()
+        likelihood_proposal = norm(mu_proposal, 1).pdf(data).prod()
+        
+        # Compute prior probability of current and proposed mu        
+        prior_current = norm(mu_prior_mu, mu_prior_sd).pdf(mu_current)
+        prior_proposal = norm(mu_prior_mu, mu_prior_sd).pdf(mu_proposal)
+        
+        p_current = likelihood_current * prior_current
+        p_proposal = likelihood_proposal * prior_proposal
+        
+        # Accept proposal?
+        p_accept = p_proposal / p_current
+        
+        # Usually would include prior probability, which we neglect here for simplicity
+        accept = np.random.rand() < p_accept
+        
+        if plot:
+            plot_proposal(mu_current, mu_proposal, mu_prior_mu, mu_prior_sd, data, accept, posterior, i)
+        
+        if accept:
+            # Update position
+            mu_current = mu_proposal
+        
+        posterior.append(mu_current)
+        
+    return posterior
+
+# Function to display
+def plot_proposal(mu_current, mu_proposal, mu_prior_mu, mu_prior_sd, data, accepted, trace, i):
+    from copy import copy
+    trace = copy(trace)
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(ncols=4, figsize=(16, 4))
+    fig.suptitle('Iteration %i' % (i + 1))
+    x = np.linspace(-3, 3, 5000)
+    color = 'g' if accepted else 'r'
+        
+    # Plot prior
+    prior_current = norm(mu_prior_mu, mu_prior_sd).pdf(mu_current)
+    prior_proposal = norm(mu_prior_mu, mu_prior_sd).pdf(mu_proposal)
+    prior = norm(mu_prior_mu, mu_prior_sd).pdf(x)
+    ax1.plot(x, prior)
+    ax1.plot([mu_current] * 2, [0, prior_current], marker='o', color='b')
+    ax1.plot([mu_proposal] * 2, [0, prior_proposal], marker='o', color=color)
+    ax1.annotate("", xy=(mu_proposal, 0.2), xytext=(mu_current, 0.2),
+                 arrowprops=dict(arrowstyle="->", lw=2.))
+    ax1.set(ylabel='Probability Density', title='current: prior(mu=%.2f) = %.2f\nproposal: prior(mu=%.2f) = %.2f' % (mu_current, prior_current, mu_proposal, prior_proposal))
+    
+    # Likelihood
+    likelihood_current = norm(mu_current, 1).pdf(data).prod()
+    likelihood_proposal = norm(mu_proposal, 1).pdf(data).prod()
+    y = norm(loc=mu_proposal, scale=1).pdf(x)
+    sns.distplot(data, kde=False, norm_hist=True, ax=ax2)
+    ax2.plot(x, y, color=color)
+    ax2.axvline(mu_current, color='b', linestyle='--', label='mu_current')
+    ax2.axvline(mu_proposal, color=color, linestyle='--', label='mu_proposal')
+    #ax2.title('Proposal {}'.format('accepted' if accepted else 'rejected'))
+    ax2.annotate("", xy=(mu_proposal, 0.2), xytext=(mu_current, 0.2),
+                 arrowprops=dict(arrowstyle="->", lw=2.))
+    ax2.set(title='likelihood(mu=%.2f) = %.2f\nlikelihood(mu=%.2f) = %.2f' % (mu_current, 1e14*likelihood_current, mu_proposal, 1e14*likelihood_proposal))
+    
+    # Posterior
+    posterior_analytical = calc_posterior_analytical(data, x, mu_prior_mu, mu_prior_sd)
+    ax3.plot(x, posterior_analytical)
+    posterior_current = calc_posterior_analytical(data, mu_current, mu_prior_mu, mu_prior_sd)
+    posterior_proposal = calc_posterior_analytical(data, mu_proposal, mu_prior_mu, mu_prior_sd)
+    ax3.plot([mu_current] * 2, [0, posterior_current], marker='o', color='b')
+    ax3.plot([mu_proposal] * 2, [0, posterior_proposal], marker='o', color=color)
+    ax3.annotate("", xy=(mu_proposal, 0.2), xytext=(mu_current, 0.2),
+                 arrowprops=dict(arrowstyle="->", lw=2.))
+    #x3.set(title=r'prior x likelihood $\propto$ posterior')
+    ax3.set(title='posterior(mu=%.2f) = %.5f\nposterior(mu=%.2f) = %.5f' % (mu_current, posterior_current, mu_proposal, posterior_proposal))
+    
+    if accepted:
+        trace.append(mu_proposal)
+    else:
+        trace.append(mu_current)
+    ax4.plot(trace)
+    ax4.set(xlabel='iteration', ylabel='mu', title='trace')
+    plt.tight_layout()
+    #plt.legend()
 
 
 
