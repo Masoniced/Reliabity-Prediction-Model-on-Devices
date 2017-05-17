a668fdbb582558fc6790f00e24a0f1cd9e9057f0
mason <sen_mei@mymail.sutd.edu.sg>
2017-05-17 16:43:29 +0800

    update

--- FinFET Stochastic/MCMC_Reliability.py
+++ FinFET Stochastic/MCMC_Reliability.py
@@ -51,7 +51,11 @@ class MCMC:
             new_theta_beta = theta_beta + sum([cluster_data[i][k]**(alpha[i]) for k in range(len(cluster_data[i]))])
             theta[i] = np.random.gamma(new_theta_alpha, new_theta_beta)
 
-        # Update of alpha
+        # Update of alpha (sampled from Motroplis Hasting/Rejection support)
+        for i in range(num_cluster):
+            new_alpha_alpha = np.array([w_model[k] * (theta[k]*alpha[k]*data[i]**(alpha[k]-1)) * exp(-theta[k]*data[i]**(alpha[k])) for k in range(num_cluster)])
+            new_alpha_beta = np.array([w_model[j] * (theta[j]* alpha_beta[j])**(alpha[j]-1) for k in range(num_cluster)])
+
 
         
 
@@ -59,7 +63,7 @@ class MCMC:
 
 
 
-def sampler(data, samples=4, mu_init=.5, proposal_width=.5, plot=False, mu_prior_mu=0, mu_prior_sd=1.):
+def sampler1(data, samples=4, mu_init=.5, proposal_width=.5, plot=False, mu_prior_mu=0, mu_prior_sd=1.):
     mu_current = mu_init
     posterior = [mu_current]
     for i in range(samples):
@@ -89,8 +93,10 @@ def sampler(data, samples=4, mu_init=.5, proposal_width=.5, plot=False, mu_prior
             plot_proposal(mu_current, mu_proposal, mu_prior_mu, mu_prior_mu_ind, data, posterior_analytical)
         
         if accept:
+
             # Update position
-            mu_current = mu_proposal
+            mu_current += mu_proposal
+            mu_prososal = new_proposal
         
         posterior.append(mu_current)
         
@@ -107,8 +113,10 @@ def sampler(data, samples=4, mu_init=.5, proposal_width=.5, plot=False, mu_prior
         # Compute likelihood by multiplying probabilities of each data point
         likelihood_current = norm(mu_current, 1).pdf(data).prod()
         likelihood_proposal = norm(mu_proposal, 1).pdf(data).prod()
+        likelihood_proposal = norm(num_cluster, 2).pdf(data).prod()
         
-        # Compute prior probability of current and proposed mu        
+        # Compute prior probability of current and proposed mu
+        # Define the pre-clustering factor as displayed by smapler 1        
         prior_current = norm(mu_prior_mu, mu_prior_sd).pdf(mu_current)
         prior_proposal = norm(mu_prior_mu, mu_prior_sd).pdf(mu_proposal)
         
@@ -120,6 +128,7 @@ def sampler(data, samples=4, mu_init=.5, proposal_width=.5, plot=False, mu_prior
         
         # Usually would include prior probability, which we neglect here for simplicity
         accept = np.random.rand() < p_accept
+        reject -= new_proposal
         
         if plot:
             plot_proposal(mu_current, mu_proposal, mu_prior_mu, mu_prior_sd, data, accept, posterior, i)
@@ -131,6 +140,92 @@ def sampler(data, samples=4, mu_init=.5, proposal_width=.5, plot=False, mu_prior
         posterior.append(mu_current)
         
     return posterior
+
+
+def BFGS(expr, initial, tol, alpha = 1, mix_iter = 50):
+
+    var_list = list(initial.keys())
+    temp_value = list(initial.values())
+    object_func = sy.lambdify((var_list,), expr, 'numpy', dummify=False)    
+    gradient = [sy.diff(expr, i) for i in var_list]
+    gradient_func = sy.lambdify((var_list,), gradient, 'numpy', dummify=False)
+
+    error = 1
+    I_ini = np.identity(len(var_list))
+    hessian_inv = I_ini
+    count = 0
+
+    #import pdb; pdb.set_trace()
+
+
+    while error >= tol:
+
+        g = np.array(gradient_func(temp_value))
+        p = -1.0 * np.dot(hessian_inv, g)
+        p = p / np.dot(p,p)
+        step = step_checking(object_func, gradient_func, p, temp_value, 1e3, tol)
+        s = step * p
+        if step == 0:
+            count += 1
+            break
+
+        new_value = temp_value + s
+        new_g = np.array(gradient_func(new_value))
+        y = new_g - g
+        # Define the posterior density 
+        if np.dot(s,y) > 0:
+            by_product =  np.dot(np.transpose([s]),[s]) / np.dot(s,y) - np.dot(np.dot(hessian_inv,np.transpose([y])), np.dot([y],hessian_inv)) / np.dot([y], np.dot(hessian_inv,np.transpose([y])))
+        else:
+            by_product = 0
+
+
+        hessian_inv = hessian_inv - by_product
+        temp_value = new_value
+        error = sqrt(np.dot(new_g, new_g))
+
+        count += 1
+
+        if count > mix_iter:
+            break
+
+
+    return dict(zip(var_list, temp_value)), error, count, object_func(temp_value)
+
+
+
+def step_checking(f, g, d, x, ini_alpha, tol, mixiter=50):
+    rho = 0.2
+    sigma = 1/6
+    step_criteria = 0
+    alpha = ini_alpha
+    final_a = 1
+    count = 0
+
+    #import pdb; pdb.set_trace()
+
+    while step_criteria == 0:
+        count += 1
+        C0 = f(x + d*alpha)
+        C1 = f(x) + alpha*rho*np.dot(g(x),d)
+        C2 = np.dot(g(x + d*alpha),d)
+        C3 = sigma*np.dot(g(x),d)
+        if C0 <= C1:
+
+            if C2 >= C3:
+                final_a = alpha
+                step_criteria = 1
+            else:
+                alpha = alpha/rho
+            
+        else:
+            alpha = alpha*rho/sigma
+
+        if count > mixiter:
+            break
+
+
+    return final_a
+
 
 # Function to display
 def plot_proposal(mu_current, mu_proposal, mu_prior_mu, mu_prior_sd, data, accepted, trace, i):
@@ -148,8 +243,7 @@ def plot_proposal(mu_current, mu_proposal, mu_prior_mu, mu_prior_sd, data, accep
     ax1.plot(x, prior)
     ax1.plot([mu_current] * 2, [0, prior_current], marker='o', color='b')
     ax1.plot([mu_proposal] * 2, [0, prior_proposal], marker='o', color=color)
-    ax1.annotate("", xy=(mu_proposal, 0.2), xytext=(mu_current, 0.2),
-                 arrowprops=dict(arrowstyle="->", lw=2.))
+    ax1.annotate("", xy=(mu_proposal, 0.2), xytext=(mu_current, 0.2), arrowprops=dict(arrowstyle="->", lw=2.))
     ax1.set(ylabel='Probability Density', title='current: prior(mu=%.2f) = %.2f\nproposal: prior(mu=%.2f) = %.2f' % (mu_current, prior_current, mu_proposal, prior_proposal))
     
     # Likelihood
