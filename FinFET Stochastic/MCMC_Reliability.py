b6bd2b80c9c6903d63b6844580e3bfeb28b18366
mason <sen_mei@mymail.sutd.edu.sg>
2017-05-21 21:18:41 +0800

    update

--- FinFET Stochastic/MCMC_Reliability.py
+++ FinFET Stochastic/MCMC_Reliability.py
@@ -8,7 +8,86 @@ import sys
 
 class MCMC:
 
-    def MC_MX_sampler(data, burn_in, tol, num_cluster=None):  # Mixture Weibull sampler
+    def MCMC_MX_sampler(data, burn_in, test, tol, num_cluster=None):  # Mixture Weibull sampler
+
+        # Initialization
+        num_data = len(data)
+        if num_cluster == None:
+            num_cluster = 2
+        cluster_data = [[] for i in range(num_cluster)]
+        count= 0
+            
+        # Posterior Distribution sum(w_model*(theta*(s)**(alpha-1)*alpha*exp(-theta*(s)**alpha))
+        theta_alpha = 0.001
+        theta_beta = 0.001
+        alpha_alpha = 1
+        alpha_beta = 0.2
+        w_fa = np.ones(num_cluster)
+        w_model = np.random.dirichlet(w_fa)
+        theta = np.random.gamma(theta_alpha, theta_beta, num_cluster)
+        alpha = np.random.gamma(alpha_alpha, alpha_beta, num_cluster)
+
+        while count <= burn_in + test:
+
+            count += 1
+            log_likelihood = 0
+            likelihood_record = []
+            w_record = []
+            theta_record = []
+            alpha_record = []
+            # Update of P(z)
+            for i in range(num_data):
+
+                #import pdb; pdb.set_trace()
+                pz_list = np.array([w_model[k] * (theta[k]*alpha[k]*data[i]**(alpha[k]-1)) * exp(-theta[k]*data[i]**(alpha[k])) for k in range(num_cluster)])
+                total_pz = sum(pz_list)
+                if total_pz == 0:
+                    total_pz = 1
+                norm_pz = np.cumsum(pz_list)/total_pz
+                mv_pz, ind_pz = min([(mv_pz, ind_pz) for ind_pz, mv_pz in enumerate(abs(norm_pz-np.random.uniform()))])
+                cluster_data[ind_pz].append(data[i])
+                log_likelihood += log(pz_list[ind_pz])
+
+            # Update of w_model
+            #import pdb; pdb.set_trace()
+            for i in range(num_cluster):
+
+                w_fa[i] = w_model[i] + len(cluster_data[i])
+
+            w_model = np.random.dirichlet(w_fa)
+
+
+            # Update of theta
+            for i in range(num_cluster):
+
+                new_theta_alpha = len(cluster_data[i]) + theta_alpha
+                new_theta_beta = theta_beta + sum([cluster_data[i][k]**(alpha[i]) for k in range(len(cluster_data[i]))])
+                theta[i] = np.random.gamma(new_theta_alpha, new_theta_beta)
+
+            # Update of alpha (sampled from Motroplis Hasting/Rejection support)
+            for i in range(num_cluster):
+
+                import pdb; pdb.set_trace()
+                p_alpha = lambda x: x**(len(cluster_data[i]) + alpha_alpha - 1) * exp(x * sum([log(cluster_data[i][k]) for k in range(len(cluster_data[i]))]) - 
+                    theta[i] * sum([cluster_data[i][k]**(x) for k in range(len(cluster_data[i]))]) - x * alpha_beta)
+
+                new_alpha = MCMC.slice_sampler(p_alpha, alpha[i])
+                alpha[i] = new_alpha
+
+            if count >= burn_in:
+
+                w_record.append(w_model)
+                theta_record.append(theta)
+                alpha_record.append(alpha)
+
+            likelihood_record.append(log_likelihood)
+
+        return w_record, theta_record, alpha_record, likelihood_record
+
+
+
+
+    def MCMC_MC_sampler(data, burn_in, test, tol, num_cluster=None):  # Mixture of Cluster sampler
 
         # Initialization
         num_data, dim_data = np.shape(data)
@@ -34,135 +113,76 @@ class MCMC:
         theta = np.random.gamma(theta_alpha, theta_beta, num_cluster)
         alpha = np.random.gamma(alpha_alpha, alpha_beta, num_cluster)
 
-        # Update of P(z)
-        for i in range(num_data):
-            pz_list = np.array([w_model[k] * (theta[k]*alpha[k]*data[i]**(alpha[k]-1)) * exp(-theta[k]*data[i]**(alpha[k])) for k in range(num_cluster)])
-            pz_list = np.cumsum(pz_list)/sum(pz_list)
-            mv_pz, ind_pz = min([(mv_pz, ind_pz) for ind_pz, mv_pz in enumerate(abs(pz_list-np.random.uniform()))])
-            cluster_data[ind_pz].append(data[i])
+        while count <= burn_in + test:
 
-        # Update of w_model
-        for i in range(num_cluster):
-            w_model[i] = np.random.dirichlet(w_fa[i] + len(cluster_data[i]))
+            count += 1
+            log_likelihood = 0
+            likelihood_record = []
+            w_record = []
+            theta_record = []
+            alpha_record = []
+            # Update of P(z)
+            for i in range(num_data):
 
-        # Update of theta
-        for i in range(num_cluster):
-            new_theta_alpha = len(cluster_data[i]) + theta_alpha
-            new_theta_beta = theta_beta + sum([cluster_data[i][k]**(alpha[i]) for k in range(len(cluster_data[i]))])
-            theta[i] = np.random.gamma(new_theta_alpha, new_theta_beta)
+                pz_list = np.array([w_model[k] * (theta[k]*alpha[k]*data[i]**(alpha[k]-1)) * exp(-theta[k]*data[i]**(alpha[k])) for k in range(num_cluster)])
+                norm_pz = np.cumsum(pz_list)/sum(pz_list)
+                mv_pz, ind_pz = min([(mv_pz, ind_pz) for ind_pz, mv_pz in enumerate(abs(norm_pz-np.random.uniform()))])
+                cluster_data[ind_pz].append(data[i])
+                log_likelihood += log(pz_list[ind_pz])
 
-        # Update of alpha (sampled from Motroplis Hasting/Rejection support)
-        for i in range(num_cluster):
-            p_alpha = lambda x: x**(len(cluster_data[i]) + alpha_alpha - 1) * exp(x * sum([log(cluster_data[i][k]) for k in range(len(cluster_data[i]))]) - 
-                theta[i] * sum([cluster_data[i][k]**(x) for k in range(len(cluster_data[i]))]) - x * alpha_beta)
+            # Update of w_model
+            for i in range(num_cluster):
+
+                w_model[i] = np.random.dirichlet(w_fa[i] + len(cluster_data[i]))
+
+            # Update of theta
+            for i in range(num_cluster):
+
+                new_theta_alpha = len(cluster_data[i]) + theta_alpha
+                new_theta_beta = theta_beta + sum([cluster_data[i][k]**(alpha[i]) for k in range(len(cluster_data[i]))])
+                theta[i] = np.random.gamma(new_theta_alpha, new_theta_beta)
+
+            # Update of alpha (sampled from Motroplis Hasting/Rejection support)
+            for i in range(num_cluster):
+
+                p_alpha = lambda x: x**(len(cluster_data[i]) + alpha_alpha - 1) * exp(x * sum([log(cluster_data[i][k]) for k in range(len(cluster_data[i]))]) - 
+                    theta[i] * sum([cluster_data[i][k]**(x) for k in range(len(cluster_data[i]))]) - x * alpha_beta)
+
+                new_alpha = slice_sampler(p_alpha, alpha[i])
+                alpha[i] = new_alpha
+
+            if count >= burn_in:
+
+                w_record.append(w_model)
+                theta_record.append(theta)
+                alpha_record.append(alpha)
+
+            likelihood_record.append(log_likelihood)
+
+        return w_record, theta_record, alpha_record, likelihood_record
 
 
-        return
-
-
-
-    def slicing_sampler(pdf, current_value, support=1e3): 
+    def slice_sampler(pdf, current_value, support=1e3, limit=3e2): 
         
         P = pdf
         criteria = 1
         Uni_top = P(current_value)
-        while criteria = 1:
+        count = 0
+        while criteria == 1:
 
-            r = np.random.uniform(0, Uni_top)
+            count += 1
+            r = support*np.random.uniform(0, 1)
+            if P(r) >= Uni_top:
+                new_sample = r
+                criteria = 0
+                break
 
+            if count > limit:
+                count = 0
+                support = support * 10
 
+        return new_sample
 
-
-
-
-
-
-
-
-        
-
-
-
-
-
-def sampler1(data, samples=4, mu_init=.5, proposal_width=.5, plot=False, mu_prior_mu=0, mu_prior_sd=1.):
-    mu_current = mu_init
-    posterior = [mu_current]
-    for i in range(samples):
-        # suggest new position
-        mu_proposal = norm(mu_current, proposal_width).rvs()
-
-        # Compute likelihood by multiplying probabilities of each data point
-        likelihood_current = norm(mu_current, 1).pdf(data).prod()
-        likelihood_proposal = norm(mu_proposal, 1).pdf(data).prod()
-        
-        # Compute prior probability of current and proposed mu        
-        prior_current = norm(mu_prior_mu, mu_prior_sd).pdf(mu_current)
-        posterior_proposal =  norm_hist(mu_prososal)
-        prior_proposal = norm(mu_prior_mu, mu_prior_sd).pdf(mu_proposal)
-        
-        p_current = likelihood_current * prior_current
-        p_proposal = likelihood_proposal * prior_proposal
-        
-        # Accept proposal?
-        p_accept = p_proposal / p_current
-        
-        # Usually would include prior probability, which we neglect here for simplicity
-        accept = np.random.rand() < p_accept
-        
-        if plot:
-            plot_proposal(mu_current, mu_proposal, mu_prior_mu, mu_prior_sd, data, accept, posterior, i)
-            plot_proposal(mu_current, mu_proposal, mu_prior_mu, mu_prior_mu_ind, data, posterior_analytical)
-        
-        if accept:
-
-            # Update position
-            mu_current += mu_proposal
-            mu_prososal = new_proposal
-        
-        posterior.append(mu_current)
-        posterior_analytical.append(updates)
-        
-    return posterior
-
-
-def sampler(data, samples=4, mu_init=.5, proposal_width=.5, plot=False, mu_prior_mu=0, mu_prior_sd=1.):
-    mu_current = mu_init
-    posterior = [mu_current]
-    for i in range(samples):
-        # suggest new position
-        mu_proposal = norm(mu_current, proposal_width).rvs()
-
-        # Compute likelihood by multiplying probabilities of each data point
-        likelihood_current = norm(mu_current, 1).pdf(data).prod()
-        likelihood_proposal = norm(mu_proposal, 1).pdf(data).prod()
-        likelihood_proposal = norm(num_cluster, 2).pdf(data).prod()
-        
-        # Compute prior probability of current and proposed mu
-        # Define the pre-clustering factor as displayed by smapler 1        
-        prior_current = norm(mu_prior_mu, mu_prior_sd).pdf(mu_current)
-        prior_proposal = norm(mu_prior_mu, mu_prior_sd).pdf(mu_proposal)
-        
-        p_current = likelihood_current * prior_current
-        p_proposal = likelihood_proposal * prior_proposal
-        
-        # Accept proposal?
-        p_accept = p_proposal / p_current
-        
-        # Usually would include prior probability, which we neglect here for simplicity
-        accept = np.random.rand() < p_accept
-        reject -= new_proposal
-        
-        if plot:
-            plot_proposal(mu_current, mu_proposal, mu_prior_mu, mu_prior_sd, data, accept, posterior, i)
-        
-        if accept:
-            # Update position on secondary powers 
-            mu_current = mu_proposal
-        
-        posterior.append(mu_current)
-        
-    return posterior
 
 
 def BFGS(expr, initial, tol, alpha = 1, mix_iter = 50):
@@ -250,79 +270,26 @@ def step_checking(f, g, d, x, ini_alpha, tol, mixiter=50):
     return final_a
 
 
-# Function to display
-def plot_proposal(mu_current, mu_proposal, mu_prior_mu, mu_prior_sd, data, accepted, trace, i):
-    from copy import copy
-    trace = copy(trace)
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(ncols=4, figsize=(16, 4))
-    fig.suptitle('Iteration %i' % (i + 1))
-    x = np.linspace(-3, 3, 5000)
-    color = 'g' if accepted else 'r'
-        
-    # Plot prior
-    prior_current = norm(mu_prior_mu, mu_prior_sd).pdf(mu_current)
-    prior_proposal = norm(mu_prior_mu, mu_prior_sd).pdf(mu_proposal)
-    prior = norm(mu_prior_mu, mu_prior_sd).pdf(x)
-    ax1.plot(x, prior)
-    ax1.plot([mu_current] * 2, [0, prior_current], marker='o', color='b')
-    ax1.plot([mu_proposal] * 2, [0, prior_proposal], marker='o', color=color)
-    ax1.annotate("", xy=(mu_proposal, 0.2), xytext=(mu_current, 0.2), arrowprops=dict(arrowstyle="->", lw=2.))
-    ax1.set(ylabel='Probability Density', title='current: prior(mu=%.2f) = %.2f\nproposal: prior(mu=%.2f) = %.2f' % (mu_current, prior_current, mu_proposal, prior_proposal))
-    
-    # Likelihood
-    likelihood_current = norm(mu_current, 1).pdf(data).prod()
-    likelihood_proposal = norm(mu_proposal, 1).pdf(data).prod()
-    y = norm(loc=mu_proposal, scale=1).pdf(x)
-    sns.distplot(data, kde=False, norm_hist=True, ax=ax2)
-    ax2.plot(x, y, color=color)
-    ax2.axvline(mu_current, color='b', linestyle='--', label='mu_current')
-    ax2.axvline(mu_proposal, color=color, linestyle='--', label='mu_proposal')
-    #ax2.title('Proposal {}'.format('accepted' if accepted else 'rejected'))
-    ax2.annotate("", xy=(mu_proposal, 0.2), xytext=(mu_current, 0.2),
-                 arrowprops=dict(arrowstyle="->", lw=2.))
-    ax2.set(title='likelihood(mu=%.2f) = %.2f\nlikelihood(mu=%.2f) = %.2f' % (mu_current, 1e14*likelihood_current, mu_proposal, 1e14*likelihood_proposal))
-    
-    # Posterior
-    posterior_analytical = calc_posterior_analytical(data, x, mu_prior_mu, mu_prior_sd)
-    ax3.plot(x, posterior_analytical)
-    posterior_current = calc_posterior_analytical(data, mu_current, mu_prior_mu, mu_prior_sd)
-    posterior_proposal = calc_posterior_analytical(data, mu_proposal, mu_prior_mu, mu_prior_sd)
-    ax3.plot([mu_current] * 2, [0, posterior_current], marker='o', color='b')
-    ax3.plot([mu_proposal] * 2, [0, posterior_proposal], marker='o', color=color)
-    ax3.annotate("", xy=(mu_proposal, 0.2), xytext=(mu_current, 0.2),
-                 arrowprops=dict(arrowstyle="->", lw=2.))
-    #x3.set(title=r'prior x likelihood $\propto$ posterior')
-    ax3.set(title='posterior(mu=%.2f) = %.5f\nposterior(mu=%.2f) = %.5f' % (mu_current, posterior_current, mu_proposal, posterior_proposal))
-    
-    if accepted:
-        trace.append(mu_proposal)
-    else:
-        trace.append(mu_current)
-    ax4.plot(trace)
-    ax4.set(xlabel='iteration', ylabel='mu', title='trace')
-    plt.tight_layout()
-    #plt.legend()
 
 
 
+Data_file = pd.ExcelFile(r'C:\Users\Mason\Documents\Project\Matlab Project\Clustering data processing\FinFET\125 TDDB FR0709_position.xlsx')  # revise path
+#Data_file = pd.ExcelFile(r'C:\Users\Sen\Desktop\1.xlsx')
+p_data = Data_file.parse('Sheet1', index_row = None, header = None)
+p_data.drop(p_data.columns[[0]], axis = 0, inplace  =True)  # drop first row
+p_data = p_data.iloc[:,:].values
+data = p_data[:,0]    # data from which column
+data = data.astype(np.float32, copy = False)
+data = data[~np.isnan(data)]
 
-file_name = r"C:\Users\Sen\Desktop\Raw-Curves Files\Logan-7C_w12_TDDB_25C_Compiled Raw.txt"
-File = pd.read_csv(file_name, sep ='\t', header = 0)
-Columns = File.columns
-Result = pd.DataFrame(columns = Columns)
-Ini_key = File.iat[0,2]
-criteria = 0
-Low_resistance = 1000
 
-for i in range(1, len(File.index)):
-    if File.iat[i,2] != Ini_key:
-        Ini_key = File.iat[i,2]
-        criteria = 0
-    else:
-        if criteria == 0 and File.iat[i,11] < Low_resistance:
-            Result.loc[Result.shape[0]] = File.iloc[i]
-            criteria = 1
-        else:
-            continue
+#np.seterr(divide='ignore', invalid='ignore', over='ignore')
 
-pd.DataFrame(Result).to_csv('Low_resistance'+'.txt', index=False)
+
+w_record, theta_record, alpha_record, likelihood_record = MCMC.MCMC_MX_sampler(data, burn_in=1000, test=100, tol=1e-9, num_cluster=2)
+
+plt.interactive(True)
+plt.plot(range(1100), likelihood_record, 'bo', markersize=10) 
+plt.xlabel(r'Iteration',{'fontname':'Times New Roman','fontsize':18})
+plt.ylabel(r'Loglikelihood',{'fontname':'Times New Roman','fontsize':18})
+
