48708814d284c84430761dbe7569ffee9a18819d
mason <sen_mei@mymail.sutd.edu.sg>
2017-05-25 15:12:04 +0800

    update

--- FinFET Stochastic/MCMC_Reliability.py
+++ FinFET Stochastic/MCMC_Reliability.py
@@ -83,9 +83,8 @@ class MCMC:
             
             alpha = new_alpha[:]
 
-            #if count >= burn_in:
 
-            # import pdb; pdb.set_trace()
+            # Record the sampling after burn-in
             if count >= burn_in:
                 w_record.append(w_model[:])
                 theta_record.append(theta[:])
@@ -99,77 +98,90 @@ class MCMC:
 
 
 
-    def MCMC_MC_sampler(data, burn_in, test, tol, num_cluster=None):  # Mixture of Cluster sampler
+    def MCMC_MC_sampler(data, burn_in, test, tol, num_cluster=None):  # Mixture Weibull sampler
 
         # Initialization
-        num_data, dim_data = np.shape(data)
-        cluster_index = [[] for i in range(num_cluster)]
-        cluster_data = [[] for i in range(num_cluster)]
+        num_data = len(data)
         if num_cluster == None:
             num_cluster = 2
-        ini_list = np.random.multinomial(num_data, [1/num_cluster]*num_cluster, size=1)
-        ini_index = np.arange(0, num_data, 1)
-        for i in range(num_cluster):
-            for j in range(ini_list[0, i]):
-                t_ind =np.round(np.random.uniform(0, len(ini_index)-1)).astype(int)
-                cluster_index[i].append(ini_index[t_ind])
-                ini_index = np.delete(ini_index, t_ind)
+        count= 0
             
         # Posterior Distribution sum(w_model*(theta*(s)**(alpha-1)*alpha*exp(-theta*(s)**alpha))
-        theta_alpha = 0.001
+        theta_alpha = 0.01
         theta_beta = 0.001
         alpha_alpha = 1
         alpha_beta = 0.2
         w_fa = np.ones(num_cluster)
+        origin_w_fa = w_fa[:]
         w_model = np.random.dirichlet(w_fa)
         theta = np.random.gamma(theta_alpha, theta_beta, num_cluster)
         alpha = np.random.gamma(alpha_alpha, alpha_beta, num_cluster)
+        likelihood_record = []
+        w_record = []
+        theta_record = []
+        alpha_record = []
 
-        while count <= burn_in + test:
+        while count <= (burn_in + test):
 
             count += 1
             log_likelihood = 0
-            likelihood_record = []
-            w_record = []
-            theta_record = []
-            alpha_record = []
+            cluster_data = [[] for i in range(num_cluster)]
+            new_theta = [[] for i in range(num_cluster)]
+            new_alpha = [[] for i in range(num_cluster)]
+
             # Update of P(z)
+            #import pdb; pdb.set_trace()
             for i in range(num_data):
 
                 pz_list = np.array([w_model[k] * (theta[k]*alpha[k]*data[i]**(alpha[k]-1)) * exp(-theta[k]*data[i]**(alpha[k])) for k in range(num_cluster)])
-                norm_pz = np.cumsum(pz_list)/sum(pz_list)
-                mv_pz, ind_pz = min([(mv_pz, ind_pz) for ind_pz, mv_pz in enumerate(abs(norm_pz-np.random.uniform()))])
+
+                mv_pz, ind_pz = max([(mv_pz, ind_pz) for ind_pz, mv_pz in enumerate(pz_list)])
                 cluster_data[ind_pz].append(data[i])
-                log_likelihood += log(pz_list[ind_pz])
+
+                if pz_list[ind_pz] == 0:
+                    log_likelihood += -1/tol
+                else:
+                    log_likelihood += log(pz_list[ind_pz])
 
             # Update of w_model
+            #import pdb; pdb.set_trace()
             for i in range(num_cluster):
 
-                w_model[i] = np.random.dirichlet(w_fa[i] + len(cluster_data[i]))
+                w_fa[i] = origin_w_fa[i] + len(cluster_data[i])
+
+            w_model = np.random.dirichlet(w_fa)
+
 
             # Update of theta
+            #import pdb; pdb.set_trace()
             for i in range(num_cluster):
 
                 new_theta_alpha = len(cluster_data[i]) + theta_alpha
                 new_theta_beta = theta_beta + sum([cluster_data[i][k]**(alpha[i]) for k in range(len(cluster_data[i]))])
-                theta[i] = np.random.gamma(new_theta_alpha, new_theta_beta)
+                new_theta[i] = np.random.gamma(new_theta_alpha, new_theta_beta)
+
+            theta = new_theta[:]
 
             # Update of alpha (sampled from Motroplis Hasting/Rejection support)
+            #import pdb; pdb.set_trace()
             for i in range(num_cluster):
 
                 p_alpha = lambda x: x**(len(cluster_data[i]) + alpha_alpha - 1) * exp(x * sum([log(cluster_data[i][k]) for k in range(len(cluster_data[i]))]) - 
                     theta[i] * sum([cluster_data[i][k]**(x) for k in range(len(cluster_data[i]))]) - x * alpha_beta)
 
-                new_alpha = slice_sampler(p_alpha, alpha[i])
-                alpha[i] = new_alpha
+                new_alpha[i] = MCMC.slice_sampler(p_alpha, alpha[i], left_bound=0)
+            
+            alpha = new_alpha[:]
 
-            #if count >= burn_in:
 
-            w_record.append(w_model)
-            theta_record.append(theta)
-            alpha_record.append(alpha)
+            # Record the sampling after burn-in
+            if count >= burn_in:
+                w_record.append(w_model[:])
+                theta_record.append(theta[:])
+                alpha_record.append(alpha[:])
 
             likelihood_record.append(log_likelihood)
+            
 
         return w_record, theta_record, alpha_record, likelihood_record
 
@@ -190,7 +202,9 @@ class MCMC:
         while criteria == 1:
 
             count += 1
+            # find left boundary
             left_out = MCMC.find_boundary(P, current_value, Uni_top, left, l_bound=ad_l_bound, r_bound=ad_r_bound)
+            # find right boundary
             right_out = MCMC.find_boundary(P, current_value, Uni_top, right, l_bound=ad_l_bound, r_bound=ad_r_bound)
 
             n_point = np.random.uniform(0,1) * (right_out - left_out) + left_out
@@ -402,6 +416,7 @@ Data, scale, min_value = MCMC.data_preprocessing(data)
 
 set_burn_in=10000
 w_record, theta_record, alpha_record, likelihood_record = MCMC.MCMC_MX_sampler(Data, burn_in=set_burn_in, test=100, tol=1e-9, num_cluster=3)
+import pdb; pdb.set_trace()
 plt.interactive(True)
 plt.plot(range(set_burn_in+101), likelihood_record, 'bo', markersize=10) 
 plt.xlabel(r'Iteration',{'fontname':'Times New Roman','fontsize':18})
