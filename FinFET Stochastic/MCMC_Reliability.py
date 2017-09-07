a99398f976a62c1382b1db57f44cbebb3b871aea
mason <sen_mei@mymail.sutd.edu.sg>
2017-09-07 21:53:20 +0800

    new

--- FinFET Stochastic/MCMC_Reliability.py
+++ FinFET Stochastic/MCMC_Reliability.py
@@ -4,11 +4,12 @@ import sympy as sy
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 import statsmodels.tsa.api as sm
+import pymc3 as pm
 from math import *
 
 class MCMC:
 
-    def MCMC_MX_sampler(data, burn_in, test, tol, num_cluster=None, thinning_gap=100):  # Mixture Weibull sampler
+    def MCMC_MW_sampler(data, burn_in, test, tol, num_cluster=None, thinning_gap=100):  # Mixture Weibull sampler
 
         # Initialization
         num_data = len(data)
@@ -20,8 +21,8 @@ class MCMC:
             
         # Posterior Distribution sum(w_model*(theta*(s)**(alpha-1)*alpha*exp(-theta*(s)**alpha))
         # Pramaters
-        theta_alpha = 0.01
-        theta_beta = 0.001
+        theta_alpha = 0.1
+        theta_beta = 0.01
         alpha_alpha = 1
         alpha_beta = 0.2
         w_fa = np.ones(num_cluster)
@@ -51,7 +52,7 @@ class MCMC:
             #import pdb; pdb.set_trace()
             for i in range(num_data):
 
-                pz_list = np.array([w_model[k] * (theta[k]*alpha[k]*data[i]**(alpha[k]-1)) * exp(-theta[k]*data[i]**(alpha[k])) for k in range(num_cluster)])
+                pz_list = np.array([w_model[k] * (theta[k] * alpha[k] * (data[i]**(alpha[k]-1))) * exp( - theta[k] * (data[i]**(alpha[k]))) for k in range(num_cluster)])
 
                 total_pz_list = np.cumsum(pz_list)
                 total_pz_list = total_pz_list - np.random.uniform(0,1) * total_pz_list[-1]
@@ -115,109 +116,18 @@ class MCMC:
         Autocorrelation_alpha = [sm.stattools.acf(alpha_acm[i], unbiased=True, nlags=int(np.size(w_acm)/2), fft=True) for i in range(num_cluster)]
 
       
-
         return w_record, theta_record, alpha_record, likelihood_record, Autocorrelation_w, Autocorrelation_theta, Autocorrelation_alpha
 
 
 
 
-    def MCMC_MC_sampler(data, burn_in, test, tol, num_cluster=None):  # Mixture Weibull sampler
 
-        # Initialization
-        num_data = len(data)
-        if num_cluster == None:
-            num_cluster = 2
-        count= 0
-            
-        # Posterior Distribution sum(w_model*(theta*(s)**(alpha-1)*alpha*exp(-theta*(s)**alpha))
-        theta_alpha = 0.01
-        theta_beta = 0.001
-        alpha_alpha = 1
-        alpha_beta = 0.2
-        w_fa = np.ones(num_cluster)
-        origin_w_fa = w_fa[:]
-        w_model = np.random.dirichlet(w_fa)
-        theta = np.random.gamma(theta_alpha, theta_beta, num_cluster)
-        alpha = np.random.gamma(alpha_alpha, alpha_beta, num_cluster)
-        likelihood_record = []
-        w_record = []
-        theta_record = []
-        alpha_record = []
-
-        while count <= (burn_in + test):
-
-            count += 1
-            log_likelihood = 0
-            cluster_data = [[] for i in range(num_cluster)]
-            new_theta = [[] for i in range(num_cluster)]
-            new_alpha = [[] for i in range(num_cluster)]
-
-            # Update of P(z)
-            #import pdb; pdb.set_trace()
-            for i in range(num_data):
-
-                pz_list = np.array([w_model[k] * (theta[k]*alpha[k]*data[i]**(alpha[k]-1)) * exp(-theta[k]*data[i]**(alpha[k])) for k in range(num_cluster)])
-
-                mv_pz, ind_pz = max([(mv_pz, ind_pz) for ind_pz, mv_pz in enumerate(pz_list)])
-                cluster_data[ind_pz].append(data[i])
-
-                if pz_list[ind_pz] == 0:
-                    log_likelihood += -1/tol
-                else:
-                    log_likelihood += log(pz_list[ind_pz])
-
-            # Update of w_model
-            #import pdb; pdb.set_trace()
-            for i in range(num_cluster):
-
-                w_fa[i] = origin_w_fa[i] + len(cluster_data[i])
-
-            w_model = np.random.dirichlet(w_fa)
-
-
-            # Update of theta
-            #import pdb; pdb.set_trace()
-            for i in range(num_cluster):
-
-                new_theta_alpha = len(cluster_data[i]) + theta_alpha
-                new_theta_beta = theta_beta + sum([cluster_data[i][k]**(alpha[i]) for k in range(len(cluster_data[i]))])
-                new_theta[i] = np.random.gamma(new_theta_alpha, new_theta_beta)
-
-            theta = new_theta[:]
-
-            # Update of alpha (sampled from Motroplis Hasting/Rejection support)
-            #import pdb; pdb.set_trace()
-            for i in range(num_cluster):
-
-                p_alpha = lambda x: x**(len(cluster_data[i]) + alpha_alpha - 1) * exp(x * sum([log(cluster_data[i][k]) for k in range(len(cluster_data[i]))]) - 
-                    theta[i] * sum([cluster_data[i][k]**(x) for k in range(len(cluster_data[i]))]) - x * alpha_beta)
-
-                new_alpha[i] = MCMC.slice_sampler(p_alpha, alpha[i], left_bound=0)
-            
-            alpha = new_alpha[:]
-
-
-            # Record the sampling after burn-in
-            if count >= burn_in:
-
-                w_record.append(w_model[:])
-                theta_record.append(theta[:])
-                alpha_record.append(alpha[:])
-
-            likelihood_record.append(log_likelihood)
-            
-
-        return w_record, theta_record, alpha_record, likelihood_record
-
-
-
-
-
-    def slice_sampler(pdf, current_value, step=10, left_bound=None, right_bound=None): 
+    def slice_sampler(pdf, current_value, step=3, left_bound=None, right_bound=None): 
 
         P = pdf
         criteria = 1
         Uni_top = P(current_value)
+        y = Uni_top * np.random.uniform(0,1)
         count = 0
         left = -step
         right = step
@@ -227,13 +137,13 @@ class MCMC:
 
             count += 1
             # find left boundary
-            left_out = MCMC.find_boundary(P, current_value, Uni_top, left, l_bound=ad_l_bound, r_bound=ad_r_bound)
+            left_out = MCMC.find_boundary(P, current_value, y, left, l_bound=ad_l_bound, r_bound=ad_r_bound)
             # find right boundary
-            right_out = MCMC.find_boundary(P, current_value, Uni_top, right, l_bound=ad_l_bound, r_bound=ad_r_bound)
+            right_out = MCMC.find_boundary(P, current_value, y, right, l_bound=ad_l_bound, r_bound=ad_r_bound)
 
             n_point = np.random.uniform(0,1) * (right_out - left_out) + left_out
 
-            if P(n_point) >= Uni_top:
+            if P(n_point) >= y:
                 new_sample = n_point
                 criteria = 0
                 break
@@ -327,9 +237,9 @@ class MCMC:
         data = data.astype(np.float32, copy = False)
         data = data[~np.isnan(data)]
         min_value = min(data)
-        scale = max(data) - min(data)
+        scale = max(data) 
 
-        data = (data -min_value) /  scale + shift
+        data = (data) / scale 
         
         return data, scale, min_value
 
@@ -355,18 +265,18 @@ class MCMC:
 
         summary = [(w, w_std), (theta, theta_std), (alpha, alpha_std)]
 
-        fiiting_range = np.logspace(np.log(shift), np.log(1+shift), 200, base=e)
-        real_fit_range = (fiiting_range - shift) * scale + min_value
+        fiiting_range = np.logspace(np.log(min_value / scale), np.log(1), 200, base=e)
+        real_fit_range = (fiiting_range) * scale
 
         fitting_probability = np.array([1 - sum([ w[k] * exp(- theta[k] * t ** (alpha[k])) for k in range(num_cluster)]) for t in fiiting_range])
         plot_fit_p = np.log(-np.log(1 - fitting_probability))
 
 
-
         return real_fit_range, plot_fit_p, data, plot_p, summary
 
 
-    def model_reconstruction_1(w_record, theta_record, alpha_record, raw_data):
+
+    def model_reconstruction_1(w_record, theta_record, alpha_record, raw_data, scale, shift=1e-9):
 
         num_cluster = np.size(w_record, 0)
 
@@ -385,16 +295,117 @@ class MCMC:
 
         summary = [(w, w_std), (theta, theta_std), (alpha, alpha_std)]
 
-        fiiting_range = np.logspace(np.log(min(data)), np.log(max(data)), 200, base=e)
+        fiiting_range = np.logspace(np.log(shift), np.log(1), 200, base=e)
+        real_fit_range = (fiiting_range) * scale 
 
         fitting_probability = np.array([1 - sum([ w[k] * exp(- theta[k] * t ** (alpha[k])) for k in range(num_cluster)]) for t in fiiting_range])
         plot_fit_p = np.log(-np.log(1 - fitting_probability))
 
 
 
-        return fiiting_range, plot_fit_p, data, plot_p, summary
+        return real_fit_range, plot_fit_p, data, plot_p, summary
 
 
+
+class MCPY:
+
+    def MCMC_MC_sampler(data, burn_in, test, tol, num_cluster=None):  # Mixture Weibull sampler
+
+        with pm.Model() as model:
+
+            w = pm.Dirichlet('w', np.ones_like(W))
+
+            mu = pm.Gamma('mu', 0., 10., shape=W.size)
+            tau = pm.Gamma('tau', 1., 1., shape=W.size)
+
+            x_obs = pm.NormalMixture('x_obs', w, mu, tau=tau, observed=x)
+
+
+        # Initialization
+        num_data = len(data)
+        if num_cluster == None:
+            num_cluster = 2
+        count= 0
+            
+        # Posterior Distribution sum(w_model*(theta*(s)**(alpha-1)*alpha*exp(-theta*(s)**alpha))
+        theta_alpha = 0.01
+        theta_beta = 0.001
+        alpha_alpha = 1
+        alpha_beta = 0.2
+        w_fa = np.ones(num_cluster)
+        origin_w_fa = w_fa[:]
+        w_model = np.random.dirichlet(w_fa)
+        theta = np.random.gamma(theta_alpha, theta_beta, num_cluster)
+        alpha = np.random.gamma(alpha_alpha, alpha_beta, num_cluster)
+        likelihood_record = []
+        w_record = []
+        theta_record = []
+        alpha_record = []
+
+        while count <= (burn_in + test):
+
+            count += 1
+            log_likelihood = 0
+            cluster_data = [[] for i in range(num_cluster)]
+            new_theta = [[] for i in range(num_cluster)]
+            new_alpha = [[] for i in range(num_cluster)]
+
+            # Update of P(z)
+            #import pdb; pdb.set_trace()
+            for i in range(num_data):
+
+                pz_list = np.array([w_model[k] * (theta[k]*alpha[k]*data[i]**(alpha[k]-1)) * exp(-theta[k]*data[i]**(alpha[k])) for k in range(num_cluster)])
+
+                mv_pz, ind_pz = max([(mv_pz, ind_pz) for ind_pz, mv_pz in enumerate(pz_list)])
+                cluster_data[ind_pz].append(data[i])
+
+                if pz_list[ind_pz] == 0:
+                    log_likelihood += -1/tol
+                else:
+                    log_likelihood += log(pz_list[ind_pz])
+
+            # Update of w_model
+            #import pdb; pdb.set_trace()
+            for i in range(num_cluster):
+
+                w_fa[i] = origin_w_fa[i] + len(cluster_data[i])
+
+            w_model = np.random.dirichlet(w_fa)
+
+
+            # Update of theta
+            import pdb; pdb.set_trace()
+            for i in range(num_cluster):
+
+                new_theta_alpha = len(cluster_data[i]) + theta_alpha
+                new_theta_beta = theta_beta + sum([cluster_data[i][k]**(alpha[i]) for k in range(len(cluster_data[i]))])
+                new_theta[i] = np.random.gamma(new_theta_alpha, new_theta_beta)
+
+            theta = new_theta[:]
+
+            # Update of alpha (sampled from Motroplis Hasting/Rejection support)
+            #import pdb; pdb.set_trace()
+            for i in range(num_cluster):
+
+                p_alpha = lambda x: x**(len(cluster_data[i]) + alpha_alpha - 1) * exp(x * sum([log(cluster_data[i][k]) for k in range(len(cluster_data[i]))]) - 
+                    theta[i] * sum([cluster_data[i][k]**(x) for k in range(len(cluster_data[i]))]) - x * alpha_beta)
+
+                new_alpha[i] = MCMC.slice_sampler(p_alpha, alpha[i], left_bound=0)
+            
+            alpha = new_alpha[:]
+
+
+            # Record the sampling after burn-in
+            if count >= burn_in:
+
+                w_record.append(w_model[:])
+                theta_record.append(theta[:])
+                alpha_record.append(alpha[:])
+
+            likelihood_record.append(log_likelihood)
+            
+
+        return w_record, theta_record, alpha_record, likelihood_record
 
 
 
@@ -489,12 +500,12 @@ def step_checking(f, g, d, x, ini_alpha, tol, mixiter=50):
 
 
 
-Data_file = pd.ExcelFile(r'C:\Users\Mason\Documents\Project\Matlab Project\Clustering data processing\FinFET\125 TDDB FR0709_position.xlsx')  # revise path
+Data_file = pd.ExcelFile(r'C:\Users\Mason\Documents\Project\Matlab Project\Clustering data processing\FinFET\MCMC.xlsx')  # revise path
 #Data_file = pd.ExcelFile(r'C:\Users\Sen\Desktop\1.xlsx')
 p_data = Data_file.parse('Sheet1', index_row = None, header = None)
 p_data.drop(p_data.columns[[0]], axis = 0, inplace  =True)  # drop first row
 p_data = p_data.iloc[:,:].values
-data = p_data[:,0]    # data from which column
+data = p_data[:,5]    # data from which column
 data = data.astype(np.float32, copy = False)
 data = data[~np.isnan(data)]
 Data, scale, min_value = MCMC.data_preprocessing(data)
@@ -505,13 +516,13 @@ Data, scale, min_value = MCMC.data_preprocessing(data)
 set_burn_in=1e5
 set_test=1e5
 num_cluster_set=3
-w_record, theta_record, alpha_record, likelihood_record, Autocorrelation_w, Autocorrelation_theta, Autocorrelation_alpha = MCMC.MCMC_MX_sampler(Data, burn_in=set_burn_in, test=set_test, tol=1e-9, num_cluster=num_cluster_set, thinning_gap=1000)
+w_record, theta_record, alpha_record, likelihood_record, Autocorrelation_w, Autocorrelation_theta, Autocorrelation_alpha = MCMC.MCMC_MW_sampler(Data, burn_in=set_burn_in, test=set_test, tol=1e-9, num_cluster=num_cluster_set, thinning_gap=1000)
 real_fit_range, plot_fit_p, real_data, plot_p, summary = MCMC.model_reconstruction(w_record, theta_record, alpha_record, data, scale, min_value)
-#real_fit_range, plot_fit_p, real_data, plot_p, summary = MCMC.model_reconstruction_1(w_record, theta_record, alpha_record, data)
+#real_fit_range, plot_fit_p, real_data, plot_p, summary = MCMC.model_reconstruction_1(w_record, theta_record, alpha_record, data, scale)
 
 #import pdb; pdb.set_trace()
 plt.interactive(True)
-f1, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=True)
+f1, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
 for i in range(num_cluster_set):
     ax1.plot(range(int(np.size(Autocorrelation_w[i]))), Autocorrelation_w[i],'b-')
     ax2.plot(range(int(np.size(Autocorrelation_theta[i]))), Autocorrelation_theta[i],'r-')
@@ -526,6 +537,11 @@ f2 = plt.figure()
 ax2 = f2.add_subplot(111)
 ax2.plot(real_data, plot_p, 'bo', markersize=10) 
 ax2.plot(real_fit_range, plot_fit_p, 'b-', linewidth=2)
+
+f3 = plt.figure()
+ax3 = f3.add_subplot(111)
+ax3.plot(range(len(likelihood_record)), likelihood_record, 'b-', markersize=10)
+
 
 print(summary)
 
